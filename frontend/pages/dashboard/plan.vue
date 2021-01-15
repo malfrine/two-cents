004b617733521c938d42ce4d5e996a0856b8273f
Malfrine Das <mjd423@cornell.edu>
2021-01-15 07:00:42 +0000

    Add current finances in plan view

--- frontend/pages/dashboard/plan.vue
+++ frontend/pages/dashboard/plan.vue
@@ -13,10 +13,10 @@
           <v-row align="center" align-content="center" justify="center" justify-md="space-between">
             <v-col cols="12" md="8" class="d-flex align-center justify-center justify-md-start">
               <div class="text-h4 text-sm-h3 text-md-h2 text-center text-md-start">
-                {{ planName }}
+                {{ possessiveUserName }} Financial Plan
               </div>
             </v-col>
-            <v-col v-if="!$vuetify.breakpoint.mdAndUp" cols="6" md="4">
+            <v-col v-if="!$vuetify.breakpoint.mdAndUp" cols="12" sm="8" md="4">
               <v-select
                 v-model="selectedStrategy"
                 label="Pick a Financial Plan"
@@ -70,6 +70,91 @@
               <v-expansion-panel class="my-n1">
                 <v-expansion-panel-header>
                   <div class="text-h5">
+                    Current Finances
+                  </div>
+                </v-expansion-panel-header>
+                <v-expansion-panel-content>
+                  <v-row>
+                    <v-col cols="12" md="6">
+                      <v-card class="pb-1" style="min-height: 100px">
+                        <v-card-title>
+                          {{ possessiveUserName }} Loans
+                        </v-card-title>
+                        <v-divider class="mt-n2 mb-3" />
+                        <div v-if="userHasLoans" class="py-1">
+                          <v-row v-for="loan in loans" :key="loan.id" class="px-6 my-2">
+                            <div class="text">
+                              {{ loan.name }}
+                            </div>
+                            <v-spacer />
+                            <div class="text red--text">
+                              $ {{ loan.current_balance }}
+                            </div>
+                          </v-row>
+                          <v-divider />
+                          <v-row class="px-6 my-2">
+                            <div class="text">
+                              Total Loans
+                            </div>
+                            <v-spacer class="my-2" />
+                            <div class="text red--text">
+                              $ {{ totalLoans }}
+                            </div>
+                          </v-row>
+                        </div>
+                        <div v-else>
+                          <v-container fluid>
+                            <div class="h3-text">
+                              No loans added
+                            </div>
+                          </v-container>
+                        </div>
+                      </v-card>
+                    </v-col>
+                    <v-col cols="12" md="6">
+                      <v-card class="pb-1">
+                        <v-card-title>
+                          {{ possessiveUserName }} Investments
+                        </v-card-title>
+                        <v-divider class="mt-n2 mb-3" />
+                        <div v-if="userHasInvestments" class="py-1">
+                          <v-row v-for="inv in investments" :key="inv.id" class="px-6 my-2">
+                            <div class="text">
+                              {{ inv.name }}
+                            </div>
+                            <v-spacer />
+                            <div class="text primary--text">
+                              $ {{ inv.current_balance }}
+                            </div>
+                          </v-row>
+                          <v-divider />
+                          <v-row class="px-6 my-2">
+                            <div class="text">
+                              Total Investments
+                            </div>
+                            <v-spacer class="my-2" />
+                            <div class="text primary--text">
+                              $ {{ totalInvestments }}
+                            </div>
+                          </v-row>
+                        </div>
+                        <div v-else>
+                          <v-container fluid>
+                            <div class="h2-text">
+                              No investments added
+                            </div>
+                          </v-container>
+                        </div>
+                      </v-card>
+                    </v-col>
+                  </v-row>
+                </v-expansion-panel-content>
+              </v-expansion-panel>
+            </v-container>
+            <v-container fluid>
+              <v-expansion-panel class="my-n1">
+                <v-expansion-panel-header>
+                  <div class="text-h5">
                     Milestones
                   </div>
                 </v-expansion-panel-header>
@@ -88,7 +173,7 @@
           <v-card
             max-width="344"
             style="height: 300px;"
-            class="sticky-nav"
+            class="sticky-nav mt-2"
           >
             <v-container>
               <v-select
@@ -144,9 +229,36 @@ export default {
       console.log("I'm getting strategies")
       return this.$store.getters['plan/getStrategies']
     },
-    planName () {
+    userHasLoans () {
+      return (Object.keys(this.loans)).length > 0
+    },
+    loans () {
+      return this.$store.getters['finances/getLoans']
+    },
+    userHasInvestments () {
+      return (Object.keys(this.investments)).length > 0
+    },
+    investments () {
+      return this.$store.getters['finances/getInvestments']
+    },
+    totalInvestments () {
+      let total = 0
+      for (const id in this.investments) {
+        total += this.investments[id].current_balance
+      }
+      return total
+    },
+    totalLoans () {
+      let total = 0
+      for (const id in this.loans) {
+        total += this.loans[id].current_balance
+      }
+      return total
+    },
+    possessiveUserName () {
       const firstName = this.$store.getters['finances/getFirstName']
-      return `${firstName}'s Financial Plan`
+      const posession = firstName.slice(-1) === 's' ? "'" : "'s"
+      return `${firstName}${posession}`
     },
     chartHeight () {
       switch (this.$vuetify.breakpoint.name) {
