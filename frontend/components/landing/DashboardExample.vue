70a149065bf8334f6021be8d39ea5f58d41b4775
Malfrine Das <mjd423@cornell.edu>
2021-05-29 18:48:25 +0000

    Added dashboard image placeholder during loading

--- frontend/components/landing/DashboardExample.vue
+++ frontend/components/landing/DashboardExample.vue
@@ -1,10 +1,25 @@
 <template>
   <div>
-    <v-img
-      contain
-      :src="path"
-      :max-height="maxHeight"
-    />
+    <v-sheet color="#121212" :height="maxHeight">
+      <v-container v-if="!loaded" :height="maxHeight">
+        <v-card color="#121212" :height="maxHeight">
+          <v-overlay absolute opacity="0">
+            <v-progress-circular
+              color="primary"
+              indeterminate
+            />
+          </v-overlay>
+        </v-card>
+      </v-container>
+      <v-img
+        eager
+        contain
+        :src="path"
+        :lazy-src="lazyPath"
+        :max-height="maxHeight"
+        @load="loaded = true"
+      />
+    </v-sheet>
   </div>
 </template>
 
@@ -18,7 +33,9 @@ export default {
   },
   data () {
     return {
-      path: 'https://res.cloudinary.com/two-cents-ca/image/upload/v1619809865/two-cents-dashboard-v1_ydnndl.png'
+      loaded: false,
+      path: 'https://res.cloudinary.com/two-cents-ca/image/upload/v1619809865/two-cents-dashboard-v1_ydnndl.png',
+      lazyPath: 'https://res.cloudinary.com/two-cents-ca/image/upload/v1622266837/two-cents-dashboard-v1-lazy_qzh2f9.png'
     }
   }
 }
