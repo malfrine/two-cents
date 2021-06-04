6fab819606c5c45d78098135f798335215f24d1e
Malfrine Das <mjd423@cornell.edu>
2021-06-04 04:01:11 +0000

    Made landing page nav more mobile friendly

--- frontend/layouts/landing.vue
+++ frontend/layouts/landing.vue
@@ -1,10 +1,37 @@
 <template>
   <v-app>
+    <v-navigation-drawer
+      v-if="$vuetify.breakpoint.mobile"
+      v-model="drawer"
+      right
+      app
+      dark
+    >
+      <v-list>
+        <v-list-item
+          v-for="(item, i) in items.slice().reverse()"
+          :key="i"
+          :to="item.to"
+          :href="item.href"
+          router
+          exact
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title" />
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
     <v-app-bar
       fixed
       :hide-on-scroll="!$vuetify.breakpoint.mobile"
       app
       dark
+      flat
+      style="background: #121212"
     >
       <div class="d-flex justify-center align-center">
         <NuxtLink to="/">
@@ -12,26 +39,22 @@
         </NuxtLink>
       </div>
       <v-spacer />
-      <v-btn
-        outlined
-        large
-        color="primary"
-        href="https://blog.two-cents.ca"
-        nuxt
-      >
-        Blog
-      </v-btn>
-      <!-- <v-btn
-        outlined
-        disabled
-        large
-        color="primary"
-        to="/login"
-        nuxt
-        class="mx-3"
-      >
-        Login
-      </v-btn> -->
+      <v-app-bar-nav-icon v-if="$vuetify.breakpoint.mobile" @click.stop="drawer = !drawer" />
+      <div v-else>
+        <v-btn
+          v-for="(item, i) in items"
+          :key="i"
+          :href="item.href"
+          :to="item.to"
+          :outlined="item.outlined"
+          :large="item.large"
+          color="primary"
+          nuxt
+          class="mx-1"
+        >
+          {{ item.title }}
+        </v-btn>
+      </div>
     </v-app-bar>
     <v-main>
       <nuxt />
@@ -48,6 +71,33 @@ export default {
   components: {
     BaseFooter,
     SmallLogo
+  },
+  data () {
+    return {
+      drawer: false,
+      items: [
+        {
+          icon: 'mdi-lightbulb-outline',
+          title: 'Blog',
+          href: 'https://blog.two-cents.ca',
+          outlined: true
+        },
+        {
+          icon: 'mdi-login',
+          title: 'Login',
+          to: '/login',
+          outlined: true
+        },
+        {
+          icon: 'mdi-account',
+          title: 'Register',
+          to: '/register',
+          outlined: false,
+          large: true
+        }
+
+      ]
+    }
   },
   created () {
     this.$vuetify.theme.isDark = true
